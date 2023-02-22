86a4502127cb623375e719e76ac12fd6c78b874a
fdinardo22 <fdinardo22@fed.idserve.net>
2023-02-22 12:21:07 +0100

    prova bottoni

---
+++ iWaitProject/iWaitProject/Buttons.swift
@@ -0,0 +1,20 @@
+//
+//  Buttons.swift
+//  iWaitProject
+//
+//  Created by Francesco on 22/02/23.
+//
+
+import SwiftUI
+
+struct Buttons: View {
+    var body: some View {
+        Text("Bottoni")
+    }
+}
+
+struct Buttons_Previews: PreviewProvider {
+    static var previews: some View {
+        Buttons()
+    }
+}
